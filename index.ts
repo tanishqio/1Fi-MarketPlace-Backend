4070939e08cef02989ea26a64792e20fed32bc68
tanishqio <bhakartanishq@gmail.com>
2026-09-08 00:20:48 +0530

    created db schema using prisma

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import express from "express";
-const app=express();
+const app = express();
 app.use(express.json());
 
-app.listen(3001,()=>{
+app.listen(process.env.PORT, () => {
     console.log("starter on port"
     )
 })
