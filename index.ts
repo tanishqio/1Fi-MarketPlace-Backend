75ea5fd078ea1cd0a811ae1c454b72c44177bade
tanishqio <bhakartanishq@gmail.com>
2026-09-09 20:52:26 +0530

    changed localhost in cors origin

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: [
-        "http://localhost:3005",
+        "http://localhost:3000",
         "https://frontend.vercel.app"
     ]
 }));
